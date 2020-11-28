11d63788088abac782a5b12833bdd92d992c1efc
Thomas Winter <wintert98@gmail.com>
2020-11-28 19:40:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,10 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.map do |key[i1],value[i2]|
-    data = value[i2][key] = [keyi2]
+  final_results = data.each_with_object({}) do |key, value, final_array|
+    value.each do |inner_key, name|
+      if !final_array[name]
+        final_array[name] = {}
+    end
   end
   data
 end
